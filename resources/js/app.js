cdc7cf0c559520b1cc99fc6c07415366c88ab550
shota <inoueshota0108@gmail.com>
2023-05-26 08:28:20 +0900

    Setting TailwindCSS and DaisyUI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+
+import { createApp, ref } from 'vue/dist/vue.esm-bundler';
+
+createApp({
+    setup() {
+
+        const hello = ref('Hello, Laravel10 & Vue3 !');
+
+        return {
+            hello,
+        }
+
+    },
+}).mount('#app');
