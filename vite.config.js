2c6deba929acb8c8f51811294875611739954928
S.I <134176915+s-inoue0108@users.noreply.github.com>
2023-07-01 08:50:18 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
-        vue(/*{
+        vue({
             // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
             // For example <img src="/images/logo.png"> will not work without the code below
             template: {
@@ -12,7 +12,7 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
-        }*/),
+        }),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
